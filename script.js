7f3e7dae41c19dbfdbf1d673638f745efb5dc838
Piorek1994 <pprus94@gmail.com>
2022-04-27 16:55:54 +0200

    Project finished

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('Nic tu nie ma 😱')
+const body = document.querySelector('body')
+const cards = document.querySelectorAll('.card')
+
+
+function showCard() {
+ cards.forEach(card => {
+     card.classList.remove('active')
+     this.classList.add('active')
+ })
+ 
+ handleBgColor(this)
+
+}
+
+const handleBgColor = (card) => {
+    const season = card.getAttribute('data-season')
+    body.setAttribute('data-bg', season)
+}
+
+
+
+cards.forEach(card => card.addEventListener('click', showCard))
